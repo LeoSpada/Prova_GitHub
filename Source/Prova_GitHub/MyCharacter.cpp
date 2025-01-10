bb55a3fbb348a386c9400d1b0ee2278df2bfe3c1
marioalecionci03 <marioalecionci00@gmail.com>
2025-01-10 12:15:05 +0100

    MovimentCode
    movimento in avanti e salto

---
+++ Source/Prova_GitHub/MyCharacter.cpp
@@ -0,0 +1,55 @@
+#include "MyCharacter.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "Components/InputComponent.h"
+
+AMyCharacter::AMyCharacter()
+{
+	// Abilita il tick per il personaggio
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Configura l'altezza del salto
+	GetCharacterMovement()->JumpZVelocity = JumpHeight;
+}
+
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Bind per il movimento avanti
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMyCharacter::MoveForward);
+
+	// Bind per il salto
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMyCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AMyCharacter::StopJump);
+}
+
+void AMyCharacter::MoveForward(float Value)
+{
+	if (Value != 0.0f)
+	{
+		// Aggiunge movimento avanti
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
+}
+
+void AMyCharacter::StartJump()
+{
+	bPressedJump = true; // Inizia il salto
+}
+
+void AMyCharacter::StopJump()
+{
+	bPressedJump = false; // Ferma il salto
+}
+
